0c8dbb660580eeeb565387bd58cf37effe2f3ea3
游丰(002934) <youfeng@fcbox.com>
2019-02-16 12:34:33 +0800

    模块切换

---
+++ fcbox-beedance-service/fcbox-beedance-service-sponsor/src/main/java/com/javaedge/ad/service/IAdUnitService.java
@@ -0,0 +1,37 @@
+package com.javaedge.ad.service;
+
+import com.javaedge.ad.exception.AdException;
+import com.javaedge.ad.vo.*;
+
+/**
+ * @author sss
+ * @fun 推广单元接口
+ * @date 2019/2/6
+ */
+public interface IAdUnitService {
+
+    /**
+     * @param request
+     * @return
+     * @throws AdException
+     */
+    AdUnitResponse createUnit(AdUnitRequest request) throws AdException;
+
+    /**
+     * @param request
+     * @return
+     * @throws AdException
+     */
+    AdUnitKeywordResponse createUnitKeyword(AdUnitKeywordRequest request)
+            throws AdException;
+
+    AdUnitItResponse createUnitIt(AdUnitItRequest request)
+            throws AdException;
+
+    AdUnitDistrictResponse createUnitDistrict(AdUnitDistrictRequest request)
+            throws AdException;
+
+    CreativeUnitResponse createCreativeUnit(CreativeUnitRequest request)
+            throws AdException;
+
+}
